6bfa215fd389524a821759481019e7f3055415f5
Subhojit Paul <subhojitpaul21@gmail.com>
2018-11-07 16:48:28 +0530

    technical debt fixes added, and answers endpoint renamed

--- src/bin/show_questions.rs
+++ src/bin/show_questions.rs
@@ -9,8 +9,10 @@ use diesel::prelude::*;
 use std::fmt::Write;
 
 fn index(_req: &HttpRequest) -> String {
+    // TODO: Re-implement this. Check the original app for the home page content.
     use self::schema::question::dsl::*;
 
+    // TODO: Connection should be established only once. Not per function.
     let connection = establish_connection();
     let mut output = String::new();
 
@@ -24,9 +26,10 @@ fn index(_req: &HttpRequest) -> String {
     output
 }
 
-fn submit_answer(answer: Json<AnswerForm>) -> HttpResponse {
+fn answer_post(answer: Json<AnswerForm>) -> HttpResponse {
     use schema::answers::dsl::*;
 
+    // TODO: Connection should be established only once. Not per function.
     let connection = establish_connection();
 
     diesel::insert_into(answers)
@@ -38,9 +41,10 @@ fn submit_answer(answer: Json<AnswerForm>) -> HttpResponse {
 }
 
 fn main() {
+    // TODO: URL should come from environment variable.
     server::new(|| App::new()
         .resource("/", |r| r.method(Method::GET).f(index))
-        .resource("/submit-answer", |r| r.method(Method::POST).with(submit_answer)))
+        .resource("/answers", |r| r.method(Method::POST).with(answer_post)))
         .bind("127.0.0.1:8088")
         .unwrap()
         .run();
