611f3c9d201b10dc0c0c59026d5d5992798b376d
Subhojit Paul <subhojitpaul21@gmail.com>
2018-11-08 15:52:21 +0530

    clippy

--- src/bin/index.rs
+++ src/bin/index.rs
@@ -7,7 +7,7 @@ use questionnaire_rs::*;
 
 fn main() {
     // TODO: URL should come from environment variable.
-    server::new(|| create_app())
+    server::new(create_app)
         .bind("127.0.0.1:8088")
         .unwrap()
         .run();
