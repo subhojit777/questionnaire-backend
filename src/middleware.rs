5873649b8c97b6fb0e6857606c67dfd980148254
Subhojit Paul <subhojitpaul21@gmail.com>
2019-04-17 09:22:59 +0530

    Better handling of invalid responses

--- src/middleware.rs
+++ src/middleware.rs
@@ -2,7 +2,7 @@ use crate::error::Oauth;
 use actix_web::middleware::{Middleware, Started};
 use actix_web::{Error, HttpRequest};
 use reqwest::header::AUTHORIZATION;
-use reqwest::Client;
+use reqwest::{Client, StatusCode};
 use serde_derive::*;
 
 pub struct GitHubUser;
@@ -17,17 +17,21 @@ impl<S> Middleware<S> for GitHubUser {
         if let Some(token) = req.headers().get("authorization") {
             match token.to_str() {
                 Ok(access_token) => {
+                    // TODO: Only perform a request when user id is not present in session.
                     let client = Client::new();
                     let mut response = client
                         .get("https://api.github.com/user")
                         .header(AUTHORIZATION, access_token)
-                        .send();
-                    match response {
-                        Ok(mut response_2) => {
-                            dbg!(response_2.json::<GitHubResponse>().unwrap());
-                        }
-                        Err(_) => {}
+                        .send()
+                        .expect("Unable to retrieve user id. Please check logs for details.");
+
+                    if response.status() != StatusCode::OK {
+                        return Err(Error::from(Oauth::BadRequest));
                     }
+
+                    // TODO: Store user id in session.
+                    dbg!(response.json::<GitHubResponse>().unwrap());
+
                     return Ok(Started::Done);
                 }
                 Err(_) => return Err(Error::from(Oauth::BadRequest)),
