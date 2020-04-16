9abcf75948b22816694689912ea69ade1ee2e451
Subhojit Paul <subhojitpaul21@gmail.com>
2020-04-16 09:09:17 +0530

    pass database connection to websocket for querying purpose

--- src/web_socket.rs
+++ src/web_socket.rs
@@ -1,26 +1,37 @@
+use crate::DbPool;
 use actix::prelude::*;
 use actix_http::ws::ProtocolError;
-use actix_web::web::Payload;
+use actix_web::web::{Data, Payload};
 use actix_web::HttpRequest;
 use actix_web::{get, HttpResponse};
 use actix_web_actors::ws;
 use actix_web_actors::ws::WebsocketContext;
+use diesel::r2d2::ConnectionManager;
+use diesel::r2d2::PooledConnection;
+use diesel::MysqlConnection;
 use std::time::{Duration, Instant};
 
 const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+type PooledDatabaseConnection = PooledConnection<ConnectionManager<MysqlConnection>>;
 
 struct WebSocket {
     presentation_id: i32,
     question_index: i32,
     heart_beat: Instant,
+    db_connection: PooledDatabaseConnection,
 }
 
 impl WebSocket {
-    pub fn new(presentation_id: i32, question_index: i32) -> Self {
+    pub fn new(
+        presentation_id: i32,
+        question_index: i32,
+        db_connection: PooledDatabaseConnection,
+    ) -> Self {
         Self {
             presentation_id,
             question_index,
             heart_beat: Instant::now(),
+            db_connection,
         }
     }
 
@@ -46,10 +57,14 @@ impl StreamHandler<Result<ws::Message, ProtocolError>> for WebSocket {
                 self.heart_beat = Instant::now();
                 ctx.pong(&msg);
             }
-            Ok(ws::Message::Pong(_)) => {
+            Ok(ws::Message::Pong(_msg)) => {
                 self.heart_beat = Instant::now();
             }
-            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Text(text)) => {
+                // TODO: Parse the message and return the next question index.
+                dbg!(&text);
+                ctx.text(text);
+            }
             Ok(ws::Message::Binary(_)) => println!("Unexpected binary"),
             Ok(ws::Message::Close(_)) => ctx.stop(),
             _ => ctx.stop(),
@@ -61,7 +76,9 @@ impl StreamHandler<Result<ws::Message, ProtocolError>> for WebSocket {
 pub async fn index(
     request: HttpRequest,
     stream: Payload,
+    pool: Data<DbPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let response = ws::start(WebSocket::new(-1, -1), &request, stream);
+    let connection = pool.get().expect("unable to get database connection");
+    let response = ws::start(WebSocket::new(-1, -1, connection), &request, stream);
     response
 }
